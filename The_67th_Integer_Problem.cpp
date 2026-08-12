5d32bb4b6bdd0fd2eac0179a1cb4a44488b0f0e6
Shubh kamal <crotonaldehyde938@gmail.com>
2026-08-12 22:14:03 +0530

    Updated the sol of The_67th_Integer_Problem

---
+++ The_67th_Integer_Problem.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x;
+        cin>>x;
+        cout<<x<<"\n";  // if y>=x then it will give the min=x in every case y>=x so y=x in this case min of(x,y) will be maximum.
+
+    }
+    return 0;
+
+}
