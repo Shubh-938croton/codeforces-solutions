f386f389e8e93c08d1ede0244f02f29ac846271f
Shubh kamal <crotonaldehyde938@gmail.com>
2026-08-29 18:57:25 +0530

    Updated:sol of perfect root.

--- perfect_root.cpp
+++ perfect_root.cpp
@@ -4,5 +4,17 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int 
+
+    int t;
+    cin>>t;              // the condition satisfy for all natural value;
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=1;i<=n;i++){
+            cout<<i;
+            if(i<n) cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
 }
