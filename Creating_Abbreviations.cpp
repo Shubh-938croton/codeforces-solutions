e40afa87551218a0d116439eba33462d386f4891
Shubh kamal <crotonaldehyde938@gmail.com>
2026-08-29 18:30:17 +0530

    Updated:sol of creating abbreviation.

--- Creating_Abbreviations.cpp
+++ Creating_Abbreviations.cpp
@@ -1,18 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int t;
     cin >> t;
-
-    while (t--)
-    {
-        int n,m;
-        cin>>n>>m;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+        
+        vector<string> a(n), b(m);
+        for(auto &x : a) cin >> x;
+        for(auto &x : b) cin >> x;
+        
+        vector<int> mp(26, 0);
+        for(auto &x : a){
+            mp[x.front() - 'a'] += 1;
+        }
+        
+        vector<bool> done(m, false);
+        int doneCount = 0;
+        
+        bool changed = true;
+        while(changed && doneCount < m){
+            changed = false;
+            
+            for(int i = 0; i < m; i++){
+                if(done[i]) continue;
+                
+                bool ok = true;
+                for(auto &y : b[i]){
+                    ok &= (mp[y - 'A'] > 0);
+                }
+                
+                if(ok){
+                    done[i] = true;
+                    doneCount++;
+                    mp[b[i].front() - 'A'] += 1;
+                    changed = true;
+                }
+            }
+        }
+        
+        cout << (doneCount == m ? "YES" : "NO") << "\n";
     }
-
+    
+    return 0;
 }
--- Olympiad_Date.cpp
+++ Olympiad_Date.cpp
@@ -1,17 +1,48 @@
-#include<bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // required counts for digits of "01032025"
+    int required[10] = {0};
+    string date = "01032025";
+    for(char c : date) required[c - '0']++;
+    
     int t;
-    cin>>t;
+    cin >> t;
+    
     while(t--){
         int n;
-        cin>>n;
-        for(int i=0;i<n;i++){
-            cin>>i;
+        cin >> n;
+        
+        vector<int> a(n);
+        for(int i = 0; i < n; i++) cin >> a[i];
+        
+        int count[10] = {0};
+        int ans = 0;
+        
+        for(int i = 0; i < n; i++){
+            count[a[i]]++;
+            
+            // check if all required digits satisfied
+            bool ok = true;
+            for(int d = 0; d < 10; d++){
+                if(count[d] < required[d]){
+                    ok = false;
+                    break;
+                }
+            }
+            
+            if(ok){
+                ans = i + 1; // 1-indexed step
+                break;
+            }
         }
         
+        cout << ans << "\n";
     }
+    
+    return 0;
 }
