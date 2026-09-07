2b811858e6c9fe8c79698586f072ab2dd0c9021b
Shubh kamal <crotonaldehyde938@gmail.com>
2026-09-07 19:32:01 +0530

    Updated the sol of cake levelling .

--- Cake_levelling.cpp
+++ Cake_levelling.cpp
@@ -6,11 +6,18 @@ int main(){
     while(t--){
         int n;
         cin>>n;
-        vector<int> arr;
-        for(int i=0;i<n;i++){
-            cin>>arr[i];
+        vector<long long> arr(n);
+        for(auto &x : arr) cin>>x;
+        long long  ans=INT_MAX;
+        long long sum=0;
+        for(int i=1;i<n+1;i++){
+            sum+=arr[i-1];
+            ans=min(ans,(sum/(i)));
+            cout<<ans<<" ";
         }
 
+        cout<<"\n";
         
     }
+    return 0;
 }
